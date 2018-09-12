6c0ec95bd956c431c109719d6f69c32744841a33
Bogdanov Ilya <bogdanov@comepay.ru>
2018-09-12 16:34:37 +0300

    fix

--- src/app/main/main.component.jsx
+++ src/app/main/main.component.jsx
@@ -7,21 +7,11 @@ export default class MainComponent extends React.Component {
     render() {
         return (
             <div className="main">
-<<<<<<< HEAD
                 <img src="/assets/img/img-1.jpg" alt="img"/>
                 <img src="/assets/img/img-2.jpg" alt="img"/>
                 <img src="/assets/img/img-3.jpg" alt="img"/>
                 <img src="/assets/img/img-4.png" alt="img"/>
                 <img src="/assets/img/img-5.gif" alt="img"/>
-=======
-                <h1>Main title downloading</h1>
-                <img src="/favicon.png" alt="img"/>
-                <img src="/favicon.png" alt="img"/>
-                <img src="/favicon.png" alt="img"/>
-                <img src="/favicon.png" alt="img"/>
-                <img src="/favicon.png" alt="img"/>
-                <img src="/favicon.png" alt="img"/>
->>>>>>> c576f9f
             </div>
         );
     }
